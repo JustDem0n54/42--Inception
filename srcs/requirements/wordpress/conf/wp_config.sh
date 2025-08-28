28af27721fda6f9bece9adb288ef162bdf1da290
Noah Rontard <rontard.noah@gmail.com>
2025-08-28 17:13:56 +0200

    not working ahah

---
+++ srcs/requirements/wordpress/conf/wp_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 10
+
+wp config create	--allow-root \
+	--dbname=$MYSQL_DATABASE \
+	--dbuser=$MYSQL_USER \
+	--dbpass=$MYSQL_PASSWORD \
+	--dbhost=mariadb:3306 --path='/var/www/wordpress'
