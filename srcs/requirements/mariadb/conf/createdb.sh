631ca11430e85a7eda90207750fefc747ccded0e
NoahRontard <rontard.noah@gmail.com>
2025-09-12 17:39:04 +0200

    finish

--- srcs/requirements/mariadb/conf/createdb.sh
+++ srcs/requirements/mariadb/conf/createdb.sh
@@ -3,48 +3,39 @@ set -e
 
 DATADIR="/var/lib/mysql"
 
-check_mariadb_initialized() {
-    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "SHOW TABLES;" &>/dev/null
-}
-
-if [ -d "$DATADIR/mysql" ] && check_mariadb_initialized(); then
-  echo "MariaDB already created"
-  
-  mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-  mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
-
-  echo "Start new mariadb with password and network"
-  mysqld_safe --datadir="$MYSQL_DATA_DIR" --skip-grant-tables &
-  
+if [ -f /var/lib/mysql/.mariadb_initialized ]; then
+  echo "MariaDB already created✅"
+  exec mysqld_safe --datadir="$DATADIR"
 else
-
-  echo "Mariadb initialisation"
+  echo "Initialisation of mariadb..."
   mysql_install_db --user=mysql --datadir="$DATADIR"
-
-  echo "Start mariadb without network"
+  
+  echo "First start without network"
   mysqld_safe --skip-networking &
   pid=$!
-
+  
   echo "Waiting for mariadb to be ready"
   until mysqladmin ping --silent; do
     sleep 1
   done
-
-  echo "Database initialisation"
+  
+  echo "Creation of the database... 🔨"
   mysql -u root <<-EOSQL
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+    CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
     CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
     CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
     GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-    CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-    GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
     FLUSH PRIVILEGES;
 EOSQL
 
   echo "Stop mariadb"
   mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
   wait $pid
-
-  echo "Start new mariadb with password and network"
+  
+  touch /var/lib/mysql/.mariadb_initialized
+  
+  echo "Launching MariaDB 🚀"
   exec mysqld_safe --datadir="$DATADIR" --skip-name-resolve
 fi
